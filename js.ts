d578e6f62ccbb3a579ca724e6c3f2952658ffaba
saikiran <35389534+sai1004@users.noreply.github.com>
2019-04-02 09:32:25 +0530

    Update js.ts

--- js.ts
+++ js.ts
@@ -1,1 +1,22 @@
 
+const items = [
+
+  { name: 'bike', price:100 },
+  { name: 'tv', price:10000 },  
+  { name: 'car', price:1000000 },  
+  { name: 'mobile', price:10000 },  
+  { name: 'laptop', price:25000 },  
+  { name: 'pen', price:20 },  
+  { name: 'book', price:45 },  
+  { name: 'adobe', price:99 },  
+
+]
+
+// method for filtering items this will print items which are price less than 100
+const fliterdItems = items.filter((item) => {
+         return item.price <= 100
+  
+})
+
+console.log(items)
+console.log(filteredItems)
