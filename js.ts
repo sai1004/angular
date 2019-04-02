bd29c942b68a33b275489edbc2f04aad424df1e2
saikiran <35389534+sai1004@users.noreply.github.com>
2019-04-02 09:59:50 +0530

    Update js.ts

--- js.ts
+++ js.ts
@@ -1,3 +1,5 @@
+//https://www.youtube.com/watch?v=R8rmfD9Y5-c
+
 
 const items = [
 
@@ -20,3 +22,120 @@ const fliterdItems = items.filter((item) => {
 
 console.log(items)
 console.log(filteredItems)
+
+
+
+const items = [
+
+  { name: 'bike', price:100 },
+  { name: 'tv', price:10000 },  
+  { name: 'car', price:1000000 },  
+  { name: 'mobile', price:10000 },  
+  { name: 'laptop', price:25000 },  
+  { name: 'pen', price:20 },  
+  { name: 'book', price:45 },  
+  { name: 'adobe', price:99 },  
+
+]
+
+// method for mapping items this will print only price of items
+
+const itemNames = items.map((item) => {
+         return item.price
+})
+
+console.log(items)
+console.log(itemNames)
+
+//===================================================================
+
+const items = [
+
+  { name: 'bike', price:100 },
+  { name: 'tv', price:10000 },  
+  { name: 'car', price:1000000 },  
+  { name: 'mobile', price:10000 },  
+  { name: 'laptop', price:25000 },  
+  { name: 'pen', price:20 },  
+  { name: 'book', price:45 },  
+  { name: 'adobe', price:99 },  
+
+]
+
+// method for finding items
+
+const foundItem = items.find((item) => {
+         return item.name === 'book'
+})
+
+console.log(items)
+console.log(foundItem)
+
+//===================================================================
+
+const items = [
+
+  { name: 'bike', price:100 },
+  { name: 'tv', price:10000 },  
+  { name: 'car', price:1000000 },  
+  { name: 'mobile', price:10000 },  
+  { name: 'laptop', price:25000 },  
+  { name: 'pen', price:20 },  
+  { name: 'book', price:45 },  
+  { name: 'adobe', price:99 },  
+
+]
+
+// method for forEach
+
+items.forEach((item) => {
+  
+   console.log(items.price)
+})
+
+//===========================================================================
+
+
+// method for some 
+ 
+const hasInexpensiveItems = items.some((item) => {
+  
+   return item.price <= 0
+})
+
+console.log(hasInexpensiveItems)
+
+//===========================================================================
+
+
+// method for every 
+
+const hasInexpensiveItems = items.every((item) => {
+  
+   return item.price <= 1000
+})
+
+console.log(hasInexpensiveItems)
+
+//===========================================================================
+
+
+// method for reduce 
+
+
+const total = items.reduce((currentTotal, item) => {
+  
+   return item.price + currentTotal
+  
+},0)
+
+console.log(total)
+
+//===========================================================================
+
+// method for includes
+const items = [1,2,3,4,5,6]
+
+const includesTwo = items.includes(7)
+
+console.log(includesTwo)
