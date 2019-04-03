3e985ac0d925518e8b3095352f3aa6df10ac926a
saikiran <35389534+sai1004@users.noreply.github.com>
2019-04-03 09:00:24 +0530

    Update jsfiltermethods.ts

--- jsfiltermethods.ts
+++ jsfiltermethods.ts
@@ -23,20 +23,9 @@ const fliterdItems = items.filter((item) => {
 console.log(items)
 console.log(filteredItems)
 
+//===================================================================
 
-
-const items = [
-
-  { name: 'bike', price:100 },
-  { name: 'tv', price:10000 },  
-  { name: 'car', price:1000000 },  
-  { name: 'mobile', price:10000 },  
-  { name: 'laptop', price:25000 },  
-  { name: 'pen', price:20 },  
-  { name: 'book', price:45 },  
-  { name: 'adobe', price:99 },  
-
-]
+ 
 
 // method for mapping items this will print only price of items
 
@@ -48,19 +37,7 @@ console.log(items)
 console.log(itemNames)
 
 //===================================================================
-
-const items = [
-
-  { name: 'bike', price:100 },
-  { name: 'tv', price:10000 },  
-  { name: 'car', price:1000000 },  
-  { name: 'mobile', price:10000 },  
-  { name: 'laptop', price:25000 },  
-  { name: 'pen', price:20 },  
-  { name: 'book', price:45 },  
-  { name: 'adobe', price:99 },  
-
-]
+ 
 
 // method for finding items
 
@@ -73,18 +50,7 @@ console.log(foundItem)
 
 //===================================================================
 
-const items = [
-
-  { name: 'bike', price:100 },
-  { name: 'tv', price:10000 },  
-  { name: 'car', price:1000000 },  
-  { name: 'mobile', price:10000 },  
-  { name: 'laptop', price:25000 },  
-  { name: 'pen', price:20 },  
-  { name: 'book', price:45 },  
-  { name: 'adobe', price:99 },  
-
-]
+ 
 
 // method for forEach
 
