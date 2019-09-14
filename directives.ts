dbfc015693ba219d3cab01f2575cd785093ef339
saikiran <35389534+sai1004@users.noreply.github.com>
2019-09-14 16:12:52 +0530

    Create directives.ts

---
+++ directives.ts
@@ -0,0 +1,41 @@
+ 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dom-events',
+  templateUrl:`
+ 
+   <h1> Types Of Directives </h1>
+  
+  <ul>
+  <li> Component <li>
+  <p> '<bs-app></bs-app>' </p>
+  
+  <li> Structural  <li>
+  
+  <p> NgIf, NgFor, NgStyle </p>
+  
+  <li> Attribute <li>
+
+  <p highlight>  in-line attribute </p>
+  
+  </ul>
+  
+  
+  
+  `
+})
+export class OnewayDbinding implements OnInit {
+ 
+ showImg: boolean = true;
+ 
+ hideImg():void {  // this void method wont return a value
+  this.showImg = !this.showImg
+ }
+ 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
