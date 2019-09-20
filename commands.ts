028e7e71c2335464d91e760f3f02a60936187b2a
saikiran <35389534+sai1004@users.noreply.github.com>
2019-09-21 00:01:06 +0530

    Create commands.ts

---
+++ commands.ts
@@ -0,0 +1,44 @@
+https://lzone.de/cheat-sheet/Nodejs
+https://downloads.gosquared.com/help_sheets/11/Node%20Help%20Sheet.pdf
+http://react.tips/
+https://gist.github.com/LeCoupa/985b82968d8285987dc3
+http://overapi.com/nodejs
+https://www.tutorialspoint.com/nodejs/nodejs_tutorial.pdf
+https://riptutorial.com/Download/node-js.pdf
+https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.jvandemo.com%2Fcontent%2Fimages%2F2015%2FMar%2Fnew-angular-router.png&imgrefurl=https%3A%2F%2Fflipboard.com%2F%40ncornag&docid=E4d6SiCTUXwZIM&tbnid=6Xna2Yg_y_1xYM%3A&vet=12ahUKEwi8hI_zguDkAhW-6nMBHcwrA5o4ZBAzKA0wDXoECAEQDg..i&w=1920&h=1080&itg=1&safe=active&bih=602&biw=1366&q=nodejs%20cheatsheet&ved=2ahUKEwi8hI_zguDkAhW-6nMBHcwrA5o4ZBAzKA0wDXoECAEQDg&iact=mrc&uact=8
+
+
+
+npm list        # Show packages in local path
+npm list -g     # Show global packages
+npm list -g -p      # Show global packages, parseble list
+
+npm view <package>
+
+npm install <package>[@<version>]
+npm uninstall <package>[@<version>]
+npm rm <package>
+
+npm search <something>
+
+npm link <package>    # Link global package into user env
+npm unlink <package>
+
+npm link        # Link package into global env (when in package dir)
+npm unlink
+
+
+Development
+
+npm init        # Create new package
+
+npm publish
+npm unpublish <package>
+
+npm adduser
+
+npm owner add <user> <package>
+npm owner rm <user> <package>
+npm owner ls <package>
+
+npm start       # Start an app
