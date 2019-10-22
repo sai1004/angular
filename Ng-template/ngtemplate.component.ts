8b60047f9360c89bdba8a3cb2b8a12f15f0192c1
saikiran <35389534+sai1004@users.noreply.github.com>
2019-10-22 08:15:11 +0530

    Update ngtemplate.component.ts

--- Ng-template/ngtemplate.component.ts
+++ Ng-template/ngtemplate.component.ts
@@ -2,26 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `
-    <ng-container *ngTemplateOutlet="templateRef; context: exampleContext"></ng-container>
-    <ng-template #templateRef let-default let-other="aContextProperty">
-      <div>
-        $implicit = '{{default}}'
-        aContextProperty = '{{other}}'
-      </div>
-    </ng-template>
+
+<section *ngIf=' course.length > 0; then recordsFound else noRecoredFound'></section>
+
+<ng-template #recordsFound>
+		<h4>recored found</h4>
+</ng-template>
+
+<ng-template #noRecoredFound>
+    <h4>recored not found</h4>
+</ng-template>
+
 `
 })
 export class NgTemplateOutletExample {
-  exampleContext = {
-    $implicit: 'default context property when none specified',
-    aContextProperty: 'a context property'
-  };
+     course = [1,2,3]
+
 }
 
 
-// output:
-
-// <div>
-//   $implicit = 'default context property when none specified'
-//   aContextProperty = 'a context property'
-// </div>
+ 
