b2156a37fd04e4a4dcd6b51ce2bb4f8713e2fee0
saikiran <35389534+sai1004@users.noreply.github.com>
2020-02-11 09:22:35 +0530

    Create first.component.ts

---
+++ 01.chapters/chapter01/first.component.ts
@@ -0,0 +1,21 @@
+import { Component , OnInit } from '@angular/core';
+
+@Component({
+
+  selector: "app-first",
+  templateUrl: `
+  
+  <h1> Hello I Am First Component </h1>
+  
+  `,
+ 
+
+})
+
+export class FirstComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
