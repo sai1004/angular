6714923e9fa70596f87ad70f08bc8cea79f98018
saikiran <35389534+sai1004@users.noreply.github.com>
2020-03-10 23:55:10 +0530

    Create pipes.ts

---
+++ 01.chapters/chapter02/pipes.ts
@@ -0,0 +1,42 @@
+import { Component  } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl:  `
+  
+   <h4> {{ name | uppercase }} </h4>
+   
+   <h4> {{ name | lowercase }}  </h4>
+   
+   <h4> {{ name | titlecase }}  </h4>
+   
+   <h4> {{ name | slice3:5 }} </h4>
+   
+   <h4>  {{ 5.678 | number:"1.2-3" }} </h4> // 5. == 1
+   <h4>  {{ 5.678 | number:"3.2-3" }} </h4> //005. == 3
+    
+    <h4> percentage: </h4>
+    <h4>  {{ 5.678 | percent }} </h4>
+  
+   <h4> currency </h4>
+      <h4>  {{ 123 | currency }} </h4>
+      <h4>  {{ 123 | currency:"GBP" }} </h4>
+      <h4>  {{ 123 | currency:"IND" }} </h4>
+      
+    <h4>Date pipe:</h4>
+ 
+   <h4>  {{ date }} </h4>
+   
+   <h4>  {{ date | date:"shortDate" }} </h4>
+   <h4>  {{ date | date:"shorTime" }} </h4>
+  
+  `
+ 
+})
+
+export class AppComponent  {
+
+name = "Angular"
+date = new Date()
+
+}
